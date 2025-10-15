eb552c0fc7b54e088bfde745b489b0b03fe2a971
femtoF <yukhym.kashchenko@gmail.com>
2025-10-15 19:11:06 +0300

    feat: changed

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import { createTheme, ThemeProvider } from "@mui/material";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import AppRoutes from "./router";
@@ -5,11 +6,18 @@ import AppRoutes from "./router";
 const queryClient = new QueryClient();
 
 function App() {
+  const theme = createTheme({
+    typography: {
+      fontFamily: "'Nunito', sans-serif",
+    },
+  });
   return (
-    <QueryClientProvider client={queryClient}>
-      <ReactQueryDevtools />
-      <AppRoutes />
-    </QueryClientProvider>
+    <ThemeProvider theme={theme}>
+      <QueryClientProvider client={queryClient}>
+        <ReactQueryDevtools />
+        <AppRoutes />
+      </QueryClientProvider>
+    </ThemeProvider>
   );
 }
 
